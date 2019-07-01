1b6f2aac5c13530b0108cc5e9325e6ea8b17ad82
Aditya Kumar Jha <adityajha126@gmail.com>
2019-07-01 15:19:33 +0530

    product name shown

--- scrapper.py
+++ scrapper.py
@@ -1,8 +1,16 @@
 import requests 
 
+from bs4 import BeautifulSoup
+
 URL = 'https://www.amazon.de/Sony-Digitalkamera-Touch-Display-Vollformatsensor-Kartenslots/dp/B07B4L1PQ8/ref=sr_1_1_sspa?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=sony+alfa+7+ILCE&qid=1561962786&s=gateway&sr=8-1-spons&psc=1'
 
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
 
 page = requests.get(URL, headers= headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+title = soup.find(id="productTitle").get_text()
+
+print(title.strip())
